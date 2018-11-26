44d1b14c05d44633c387e6fd2d460890a67e60cc
Sithixay Douangchak <>
2018-11-26 14:14:20 +0700

    Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -12,6 +12,11 @@ contract DappToken {
 
     uint256 public totalSupply;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
     mapping(address => uint256) public balanceOf;
 
@@ -21,6 +26,22 @@ contract DappToken {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
         // Allocate the initial supply
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        // Exception if account doesn't hae enough balance
+        require(balanceOf[msg.sender] >= _value);
+        
+        // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        // Transfer Event
+        Transfer(msg.sender, _to, _value);
+
+        // Return a boolean
+        return true;   
     }
 
 
